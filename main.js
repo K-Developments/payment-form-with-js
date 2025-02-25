e45d37b0eb77519c33e5db7f0cc9aece7999211d
Kaveen <kaveenthilakarathna582@gmail.com>
2025-02-25 19:27:09 +0530

    main.js

--- main.js
+++ main.js
@@ -9,21 +9,20 @@ const out2 = document.getElementById("demo2");
 btn.onclick = function() {
   if(check.checked) {
     out.textContent = ("Thanks for read terms & conditions");
-
-    switch(check.checked){
-      case visa.checked:
-        out2.textContent = "Your payment proceed with visa";
-        break;
-      case master.checked:
-        out2.textContent = "Your payment proceed with master";
-        break;
-      case paypal.checked:
-        out2.textContent = "Payment with paypal";
-        break;
-      default:
-        out2.textContent = "Select your payment method"; 
-    };
   }else{
     out.textContent = "Please read terms and conditions";
   }
+  switch(check.checked){
+    case visa.checked:
+      out2.textContent = "Your payment proceed with visa";
+      break;
+    case master.checked:
+      out2.textContent = "Your payment proceed with master";
+      break;
+    case paypal.checked:
+      out2.textContent = "Payment with paypal";
+      break;
+    default:
+      out2.textContent = "Select your payment method"; 
+  };
 }
